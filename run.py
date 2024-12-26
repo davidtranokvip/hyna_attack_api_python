d82287010b17541bfabbfc7b5576e6299fe8a371
davidtranokvip <davidtran@okvip.com>
2024-12-26 17:05:32 +0700

    chore: add cors

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from app import create_app
 from app.db import db
 from dotenv import load_dotenv
+from flask_cors import CORS
 import os
 
 load_dotenv()
 
 app = create_app()
+CORS(app)  # Enable CORS for all routes
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI", "mysql+pymysql://username:password@localhost/dbname")
 db.init_app(app)
