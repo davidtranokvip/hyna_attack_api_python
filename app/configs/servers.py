7afb2777a4c92bce0ec0f51c107319fd3e38f77b
davidtranokvip <davidtran@okvip.com>
2025-02-28 19:25:20 +0700

    chore: update servers

--- app/configs/servers.py
+++ app/configs/servers.py
@@ -1,16 +1,4 @@
 ATTACK_SERVERS = [
-    {
-        'hostName': '64.112.72.50',
-        'userName': 'root',
-        'password': '84Gi8tOm07Xs',
-        'nodePath': '/root/.nvm/versions/node/v20.18.3/bin/node'
-    },
-    {
-        'hostName': '91.202.4.121',
-        'userName': 'root',
-        'password': 'ISM43a7QNy9W',
-        'nodePath': '/root/.nvm/versions/node/v20.18.3/bin/node'
-    },
      {
         'hostName': '23.229.7.14',
         'userName': 'root',
