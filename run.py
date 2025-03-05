4b23f9b60ec6b00e59a06e01c1767193597f0fb4
davidtranokvip <davidtran@okvip.com>
2025-03-05 16:30:50 +0700

    update ip

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=True)
