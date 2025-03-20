8b249d1fde3510c420a1a58fc2cf42873af0475e
davidtranokvip <davidtran@okvip.com>
2025-03-20 20:30:54 +0700

    fix: update login auth

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -6,7 +6,7 @@ from app.db import db
 import jwt
 from app.configs.whitelist import WHITELISTED_IPS
 import pytz
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from app.models.user_log import UserLog
 from cryptography.hazmat.primitives import serialization
 from app.utils.decrypt_payload import decrypt_payload
@@ -24,55 +24,73 @@ with open(private_key_path, 'rb') as key_file:
 
 class AuthController:
     @staticmethod
-    def login():    
-        clientIp = request.headers.get("X-Forwarded-For", request.remote_addr)
-        if clientIp not in WHITELISTED_IPS:
-            return Response.error(message=f"{clientIp} IP Access Denied", code=400)
+    def login():
+        try:
+            clientIp = request.headers.get("X-Forwarded-For", request.remote_addr)
+            if clientIp not in WHITELISTED_IPS:
+                return Response.error(message=f"{clientIp} IP Access Denied", code=400)
 
-        data = request.get_json()
+            data = request.get_json()
+            
+            current_time = datetime.now(vn_timezone).strftime("%H:%M:%S")
 
-        current_time = datetime.now(vn_timezone).time()
+            user = User.query.filter(
+                func.binary(User.nameAccount) == data['nameAccount'],
+                and_(
+                    or_(User.entryTime.is_(None), User.entryTime <= current_time),
+                    or_(User.exitTime.is_(None), User.exitTime >= current_time)
+                )
+            ).first()
 
-        user = User.query.filter(
-            func.binary(User.nameAccount) == data['nameAccount'],
-            and_(
-                or_(User.entryTime.is_(None), User.entryTime <= current_time),
-                or_(User.exitTime.is_(None), User.exitTime >= current_time)
-            )
-        ).first()
+            if not user or not user.check_password(data['password']):
+                return Response.error("Invalid credentials or access time", code=403)
 
-        if not user or not user.check_password(data['password']):
-            return Response.error("Invalid credentials or access time", code=403)
+            if not user.isAdmin:
+                log_entry = UserLog(
+                    ip=clientIp,
+                    name_account=user.to_dict()['nameAccount'],
+                    detail="LOGIN",
+                    time_active=datetime.now(vn_timezone).strftime("%Y-%m-%d %H:%M:%S")
+                )
+                db.session.add(log_entry)
+                db.session.commit()
+            
+            token_data = AuthController.createToken(user.to_dict())
 
-        if not user.isAdmin:
-            log_entry = UserLog(
-                ip=clientIp,
-                name_account=user.nameAccount,
-                detail="LOGIN",
-                time_active=datetime.now(vn_timezone)
-            )
-            db.session.add(log_entry)
-            db.session.commit()
+            if isinstance(token_data, str):
+                return Response.error(message=token_data, code=500)
 
-        token_data = AuthController.createToken(user.to_dict())
+            return Response.success(data=token_data, message="Access Authorized")
+        except KeyError as e:
+            return Response.error(f"Missing key: {str(e)}", code=400)
 
-        if isinstance(token_data, str):
-            return Response.error(message=token_data, code=500)
-
-        return Response.success(data=token_data, message="Access Authorized")
+        except Exception as e:
+            db.session.rollback()
+            return Response.error(message=f"Internal Server Error: {str(e)}", code=500)
     
+    @staticmethod
+    def convert_datetime(obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        if isinstance(obj, list):
+            return [AuthController.convert_datetime(item) for item in obj]
+        if isinstance(obj, dict):
+            return {key: AuthController.convert_datetime(value) for key, value in obj.items()}
+        return obj
+
     @staticmethod
     def createToken(user):
         TOKEN_EXPIRATION = 600
-        expiration_time = datetime.utcnow() + timedelta(seconds=TOKEN_EXPIRATION)
-
+        expiration_time = datetime.now(timezone.utc) + timedelta(seconds=TOKEN_EXPIRATION)
+        
         SECRET_KEY = os.getenv("SECRET_KEY")
         if not SECRET_KEY:
             return "Missing SECRET_KEY in environment variables"
-
+        
         payload = vars(user) if not isinstance(user, dict) else user
         payload.pop("password", None)
         payload["exp"] = expiration_time
+        payload = AuthController.convert_datetime(payload)
 
         token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
         return {'token': token, 'expires_at': expiration_time.isoformat()}
