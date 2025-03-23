a5f51944c4c0ddf24015d9e09e2ef16769c0f0bc
davidtranokvip <davidtran@okvip.com>
2025-03-23 19:44:42 +0700

    update validate

--- app/utils/validate.py
+++ app/utils/validate.py
@@ -5,10 +5,13 @@ from app.configs.whitelist import WHITELISTED_IPS
 
 def is_whitelisted():
     client_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
+    
+    if client_ip:
+        client_ip = client_ip.split(",")[0].strip()
+    
     return client_ip in WHITELISTED_IPS
 
 def is_blacklisted(url):
     parsed_url = urlparse(url)
-    domain = parsed_url.netloc.lower().replace("www.", "")
+    domain = parsed_url.netloc.lower().lstrip("www.")
     return domain in BLACKLISTED_DOMAINS
-
